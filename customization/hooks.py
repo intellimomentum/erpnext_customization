7f5871da242365b759b5f5f84e5e4cbbd756bd88
Bernhard Hein <intellimomentum@gmail.com>
2023-01-16 11:50:19 +0100

    styles

--- customization/hooks.py
+++ customization/hooks.py
@@ -11,7 +11,7 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/customization/css/customization.css"
+app_include_css = "/assets/customization/css/customize_print.css"
 # app_include_js = "/assets/customization/js/customization.js"
 
 # include js, css files in header of web template
